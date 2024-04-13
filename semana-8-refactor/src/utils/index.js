4d2e605885c1b3f8e0560328023fc548c508d629
afgh <augustogutierrezh@gmail.com>
2024-04-13 07:21:57 -0500

    semana 8 refactor

--- semana-8-refactor/src/utils/index.js
+++ semana-8-refactor/src/utils/index.js
@@ -1,11 +1,11 @@
 export function validateIfInputIsEmpty(input) {
   if (input.value === "") {
     input.classList.add("error");
-    // error.textContent = "Debe completar este campo";
+     error.textContent = "Debe completar este campo";
     return false;
   } else {
     input.classList.remove("error");
-    // error.textContent = "";
+     error.textContent = "";
     return true;
   }
 }
